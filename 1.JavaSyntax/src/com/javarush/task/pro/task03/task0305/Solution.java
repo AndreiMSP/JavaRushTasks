9811c749e8b16b34dfbf5b0ea9e76c1da1ac9081
AndreiMSP <lanflex@mail.ru>
2024-08-07 19:11:17 +0300

    add solution java syntax task0305

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0305/Solution.java
@@ -12,18 +12,14 @@ public class Solution {
         int number1 = entrance.nextInt();
         int number2 = entrance.nextInt();
         int number3 = entrance.nextInt();
-        boolean pair1 = (number1 == number2 && number1 == number3 && number2 == number3);
-        boolean pair2 = (number1 == number3);
-        boolean pair3 = (number2 == number3);
-        boolean pair4 = (number1 == number2);
 
-        if (pair1) {
+        if (number1 == number2 && number1 == number3 && number2 == number3) {
             System.out.print(number1 + " " + number2 + " " + number3);
-        } else if (pair2)
+        } else if (number1 == number3)
             System.out.print(number1 + " " + number3);
-        else if (pair3)
+        else if (number2 == number3)
             System.out.print(number2 + " " + number3);
-        else if (pair4)
+        else if (number1 == number2)
             System.out.print(number1 + " " + number2);
 
 
