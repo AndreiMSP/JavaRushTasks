e8d0bd48273e7c806f62cf17189d541fff83aca4
AndreiMSP <lanflex@mail.ru>
2024-08-09 16:42:54 +0300

    add solution java syntax task01

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0103;
+
+/* 
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int todayyear = 3126;
+        int birthyaer = todayyear - 8;
+        System.out.println(birthyaer);
+    }
+}
