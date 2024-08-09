dd41597a836bdd8c5c97ac511fc4377486a1225a
AndreiMSP <lanflex@mail.ru>
2024-08-09 18:14:51 +0300

    add solution java syntax task0306&0309

--- 1.JavaSyntax/src/com/javarush/task/pro/task03/task0309/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0309/Solution.java
@@ -11,10 +11,16 @@ public class Solution {
         Scanner scanner = new Scanner(System.in);
         int numberA = scanner.nextInt();
         int numberB = scanner.nextInt();
-        //if (numberA > numberB) {
+/*
+        if (numberA > numberB) {
             System.out.println(numberA);
-        //} else {
+        } else {
             System.out.println(numberB);
+
         }
+
+ */
+        int result = numberA > numberB ? numberA : numberB;
+        System.out.println(result);
     }
 }
