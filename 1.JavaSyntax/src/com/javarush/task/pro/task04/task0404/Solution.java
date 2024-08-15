5676b0ecba5a51d6bffb979209b95ac2d7bb6d49
AndreiMSP <lanflex@mail.ru>
2024-08-15 12:37:12 +0300

    add solution java syntax task0404

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0404/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task04.task0404;
+
+/* 
+Заполненный прямоугольник
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int y = 0;
+        while (y < 5) {
+            y++;
+            int x = 0;
+            while (x < 10)
+            {
+            x++;
+            System.out.print("Q");
+        }
+            System.out.println("");}
+        //напишите тут ваш код
+
+    }
+}
