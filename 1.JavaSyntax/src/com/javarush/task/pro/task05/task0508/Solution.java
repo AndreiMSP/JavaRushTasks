02422094a6e24d2a3adff660f71854760ce3949f
AndreiMSP <lanflex@mail.ru>
2024-09-06 18:50:42 +0300

    add solution java syntax update tasks 0508 0509

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0508/Solution.java
@@ -11,20 +11,23 @@ public class Solution {
 
     public static void main(String[] args) {
         strings = new String[6];
-        String[] array = new String[6];
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < strings.length; i++) {
             strings[i] = scanner.nextLine();
-            array[i] = strings[i];
-        }
-        for (int i = 0; i < (strings.length); i++) {
-            for (int j = 0; j < (array.length); j++) {
 
-                if ((array[i] != null) && (strings[i].equals(strings[j]) && (i != j))) {
-                    array[i] = null;
+        }
+        for (int i = 0; i < strings.length; i++) {
+            String string = strings[i];
+            for (int j = i + 1; j < strings.length; j++) {
+                if (string == null) {
+                    break;
+                }
+                if (string.equals(strings[j])) {
+                    strings[i] = null;
+                    strings[j] = null;
                 }
             }
-            System.out.print(array[i] + ", ");
+            System.out.print(strings[i] + ", ");
         }
     }
 }
