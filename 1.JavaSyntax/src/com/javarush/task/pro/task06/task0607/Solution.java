9797444a325ce0f52e5b25ed89894ce4377e3198
AndreiMSP <lanflex@mail.ru>
2024-09-17 10:34:20 +0300

    add solution java syntax update tasks 0607

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0607/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0607/Solution.java
@@ -4,20 +4,20 @@ package com.javarush.task.pro.task06.task0607;
 Добро пожаловать! Но не всем.
 */
 
+import java.util.Scanner;
+
 public class Solution {
 
     public static void main(String[] args) {
-
+        signIn("user1");
     }
 
-    public static void signIn(String username){
-//        if (username = "user") {
-//            return;
-//        }
-//        else {
-//
-//        }
-        System.out.println("Добро пожаловать " + username);
-        System.out.println("Очень скучали по Вам, " + username);
+    public static void signIn(String username) {
+        if (username == "user") {
+            return;
+        } else {
+            System.out.println("Добро пожаловать " + username);
+            System.out.println("Очень скучали по Вам, " + username);
+        }
     }
 }
