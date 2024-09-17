72d994571efe4b40d242e3e1f34a832509b68290
AndreiMSP <lanflex@mail.ru>
2024-09-17 09:33:19 +0300

    add solution java syntax update tasks 0605 0606 0607

--- 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0605/Solution.java
@@ -1,6 +1,6 @@
 package com.javarush.task.pro.task06.task0605;
 
-/* 
+/*
 Правильный порядок
 */
 
@@ -13,18 +13,31 @@ public class Solution {
         printArray(array);
         reverseArray(array);
         printArray(array);
+
+        int sum = sum(10, 15);
+        System.out.println(sum);
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void method(String string, int number) {
+
+    }
+
+    public static void method(int number, String string) {
+
     }
 
     public static void reverseArray(int[] array) {
-        int[] reverseArray = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
-            reverseArray[array.length - 1 - i] = array[i];
+        for (int i = 0; i < array.length / 2; i++) {
+            int temp = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = temp;
+
         }
-        for (int i = 0; i < array.length; i++) {
-            array[i] = reverseArray[i];
-        }
-             // System.out.println(Arrays.toString(array));
-     }
+    }
 
     public static void printArray(int[] array) {
         for (int i : array) {
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task06/task0606/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.task.pro.task06.task0606;
+
+/* 
+Универсальный солдат
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(int a, short b) {
+
+    }
+
+    public static void universalMethod(long b, int a) {
+
+    }
+
+    public static void universalMethod(int a, int b) {
+
+    }
+
+    public static void universalMethod(int a, long b) {
+
+    }
+
+    public static void universalMethod(short b, byte c) {
+
+    }
+
+    public static void universalMethod(byte c, short a) {
+
+    }
+
+    public static void universalMethod(long a, int c, double f) {
+
+    }
+
+    public static void universalMethod(int c, double f, long a) {
+
+    }
+
+    public static void universalMethod(double f, int c, long a) {
+
+    }
+
+    public static void universalMethod(float a, int c) {
+
+    }
+}
