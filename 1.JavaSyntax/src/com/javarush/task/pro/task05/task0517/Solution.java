5ec34889fbb9fc804c65e94d599d78f522dc8e7f
AndreiMSP <lanflex@mail.ru>
2024-09-18 19:18:39 +0300

    add solution java syntax update tasks 0517

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0517/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.pro.task05.task0517;
+
+import java.util.Arrays;
+
+/* 
+Делим массив
+*/
+
+public class Solution {
+
+    public static int[][] result = new int[2][];
+    public static int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+
+    public static void main(String[] args) {
+        if (array.length % 2 != 0) {
+            int[] arrayFirst = Arrays.copyOfRange(array, 0, array.length / 2 + 1);
+            System.out.println(Arrays.toString(arrayFirst));
+            for (int i = 0; i < result.length; i++) {
+                for (int j = 0; j < result[i].length; j++) {
+                    result[j] = arrayFirst;
+                }
+
+            }
+        }
+        System.out.println(Arrays.deepToString(result));
+    }
+}
