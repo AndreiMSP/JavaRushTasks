50de9939bca5c08a41c5598863b31de377587be6
AndreiMSP <lanflex@mail.ru>
2024-09-22 11:20:36 +0300

    add solution java syntax update tasks 0702 0703 0704

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    short b = Short.MIN_VALUE;
+    long a = Long.MIN_VALUE;
+    int c = Integer.MIN_VALUE;
+    byte e = Byte.MIN_VALUE;
+
+}
