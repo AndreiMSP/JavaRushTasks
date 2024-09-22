5c174d9e97ea8757cff468d3d877f0ed442c8b26
AndreiMSP <lanflex@mail.ru>
2024-09-22 11:08:57 +0300

    add solution java syntax update tasks 0615 0616 0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int a;
+    String b;
+    long c;
+    double d;
+    byte e;
+    short f;
+    float g;
+    char h;
+    boolean j;
+}
