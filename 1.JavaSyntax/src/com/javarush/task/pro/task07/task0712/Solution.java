c81da2285db761fbe59df7312e254bff2bcf17dc
AndreiMSP <lanflex@mail.ru>
2024-09-25 18:28:40 +0300

    add solution java syntax update tasks 0711 0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,30 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        int[] array = new int[5];
+        int[] array1 = new int[5];
+        int[] array2 = new int[5];
+        int[] array3 = new int[5];
+        int[] array4 = new int[5];
+        String string = new String();
+        String string1 = new String();
+        String string2 = new String();
+        String string3 = new String();
+        String string4 = new String();
+        String string5 = new String();
+        String string6 = new String();
+        String string7 = new String();
+        String string8 = new String();
+        String string9 = new String();
+
+    }
+}
