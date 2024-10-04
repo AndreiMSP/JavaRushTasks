5407c2b212e10468b145606ae56e687ca6fc3dcf
AndreiMSP <lanflex@mail.ru>
2024-10-04 19:20:54 +0300

    add solution java syntax update tasks 0905 0910 0911 0915

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0910/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task09.task0910;
+
+/* 
+Кодировка Unicode
+*/
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        char[] symbols = new char[9];
+        init(symbols);
+        System.out.println(new String(symbols));
+        System.out.println(1<<4);
+    }
+
+    public static void init(char[] symbols) {
+        //напишите тут ваш код
+
+    }
+}
