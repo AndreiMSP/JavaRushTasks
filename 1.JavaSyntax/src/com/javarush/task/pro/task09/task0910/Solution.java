67d087d3db52aff9d1755fa1a963b8c01b35e136
AndreiMSP <lanflex@mail.ru>
2024-10-11 09:21:28 +0300

    add solution java syntax update tasks 0907 0910

--- 1.JavaSyntax/src/com/javarush/task/pro/task09/task0910/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task09/task0910/Solution.java
@@ -5,17 +5,22 @@ package com.javarush.task.pro.task09.task0910;
 */
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
         char[] symbols = new char[9];
         init(symbols);
         System.out.println(new String(symbols));
-        System.out.println(1<<4);
+        System.out.println(1 << 4);
     }
 
     public static void init(char[] symbols) {
         //напишите тут ваш код
-
+        Scanner scanner = new Scanner('\u00A9'+""+'\u004A'+'\u0061'+'\u0076'+'\u0061'+'\u0052'+'\u0075'+'\u0073'+'\u0068');
+        char[] newArray = scanner.next().toCharArray();
+        for (int i = 0; i < symbols.length; i++) {
+            symbols[i] =newArray[i];
+        }
     }
 }
