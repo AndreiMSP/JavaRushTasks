d267c26a252898a02b0a97463d5da154929ee3ab
AndreiMSP <lanflex@mail.ru>
2025-01-08 00:36:21 +0300

    add solution java syntax update tasks 1313 1316

--- 1.JavaSyntax/src/com/javarush/task/pro/task13/task1313/StringLinkedList.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1313/StringLinkedList.java
@@ -4,6 +4,13 @@ public class StringLinkedList {
     private Node first = new Node();
     private Node last = new Node();
 
+    public StringLinkedList() {
+        first.next = last;
+        last.prev = first;
+    }
+
+    ;
+
     public void printAll() {
         Node currentElement = first.next;
         while ((currentElement != null) && (currentElement != last)) {
@@ -13,7 +20,14 @@ public class StringLinkedList {
     }
 
     public void add(String value) {
-        //напишите тут ваш код
+        Node node = new Node();
+        node.value = value;
+
+
+        Node lastNode = last.prev;
+        lastNode.next = node;
+        node.prev = lastNode;
+        last.prev = node;
     }
 
     public static class Node {
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1316/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task13.task1316;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        JavarushQuest[] days = JavarushQuest.values();
+
+        for (JavarushQuest day: days)
+            System.out.println(day.ordinal());
+
+        //JavarushQuest.values();
+        //for (int i = 0; i < JavarushQuest.values(); i++) {
+//
+  //      }
+    //    System.out.println(JavarushQuest.CS_50.ordinal());
+    }
+}
