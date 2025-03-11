4633042f4045196a7ef46fbb504e66fd53dba4dd
AndreiMSP <lanflex@mail.ru>
2025-03-11 20:56:34 +0300

    add solution java syntax update tasks 1703

--- 1.JavaSyntax/src/com/javarush/task/pro/task17/task1703/Astronaut.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task17/task1703/Astronaut.java
@@ -1,7 +1,11 @@
 package com.javarush.task.pro.task17.task1703;
 
+import java.awt.*;
+
 public class Astronaut {
     public String getInfo(){
         return "Неизвестный астронавт";
     }
+
+
 }
