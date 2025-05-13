c0693ed2e6c22225f8709d8b91cc15d59f6986db
AndreiMSP <lanflex@mail.ru>
2025-05-14 00:58:07 +0300

    add solution java core update tasks 1411 1414 1420

---
+++ 2.JavaCore/src/com/javarush/task/task14/task1411/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.task14.task1411;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 
+User, Loser, Coder and Proger
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Person person = null;
+        String key = null;
+        while (true) {
+            String string = reader.readLine();
+            if(string instanceof Person.User){
+
+            }
+        }
+        //тут цикл по чтению ключей, пункт 1
+        {
+            //создаем объект, пункт 2
+
+            doWork(person); //вызываем doWork
+
+        }
+    }
+
+    public static void doWork(Person person) {
+        // пункт 3
+    }
+}
